8d7d1a3f08d34bfae67331668c7730e3540df71e
Erik Verstegen <hello@erikverstegen.com>
2016-12-12 15:46:32 +0100

    Add a start to styling the desktop page

--- src/js/desktop/main.js
+++ src/js/desktop/main.js
@@ -1,19 +1,18 @@
 var $ = require('jquery');
 
-window.onload = function() {
-
+$(window).ready(function () {
     var socket = io();
 
     var roomKey = Math.random().toString(36).substr(2, 5);
     var roomURL = location.host + '/' + roomKey;
 
-    $('#url').text(roomURL);
+    $('.js-url').text(roomURL);
 
     socket.emit('new room', {
-        key: roomKey
+        key: roomKey,
     });
 
     socket.on('user added', function(socketID, data) {
         console.log('New player: ' + socketID);
     });
-};
+});
