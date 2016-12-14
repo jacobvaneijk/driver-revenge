f72f225f4af9490c787c2ef372311348d39c268b
Erik Verstegen <hello@erikverstegen.com>
2016-12-14 11:21:30 +0100

    Add a start to changing the player names front-end

--- src/js/mobile/main.js
+++ src/js/mobile/main.js
@@ -1,4 +1,6 @@
-window.onload = function() {
+var $ = require('jquery');
+
+$(window).ready(function() {
     var socket = io();
 
     var roomKey = window.location.href.split('/')[3];
@@ -10,4 +12,12 @@ window.onload = function() {
     socket.on('room destroyed', function() {
         console.log('The room you joined was destroyed.');
     });
-};
+
+    $('.js-save').on('click', function(event) {
+        event.preventDefault();
+
+        socket.emit('change name player', $('.js-name').val());
+
+        // todo: move to next view
+    })
+});
