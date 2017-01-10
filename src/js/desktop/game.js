ec24388af8eb2cddb11dd3fb4dc523209e7ae77c
Jacob van Eijk <jacob.vaneijk@gmail.com>
2017-01-10 13:34:16 +0100

    Add basic level loader.

--- src/js/desktop/game.js
+++ src/js/desktop/game.js
@@ -4,6 +4,7 @@ module.exports = {
     renderer: null,
     socket: null,
     stage: null,
+    level: null,
 
     init: function(socket) {
         this.socket = socket;
@@ -17,16 +18,67 @@ module.exports = {
     loadLevel: function(level) {
         var self = this;
 
-        var width = Math.floor($('body').innerWidth() / level.tile_width) * level.tile_width;
-        var height = Math.floor($('body').innerHeight() / level.tile_height) * level.tile_height;
+        this.level = level;
+
+        var width = Math.floor($('body').innerWidth() / this.level.tile_width) * this.level.tile_width;
+        var height = Math.floor($('body').innerHeight() / this.level.tile_height) * this.level.tile_height;
 
         this.renderer.autoResize = true;
         this.renderer.resize(width, height);
 
-        PIXI.loader.add('background', level.background).load(function (loader, resources) {
-            var background = new PIXI.extras.TilingSprite(resources.background.texture, self.renderer.width, self.renderer.height);
+        // Load all land textures (14 in total).
+        for (var i = 1; i <= 14; ++i) {
+            PIXI.loader.add('land_' + i, 'img/land/' + this.level.land + '/land_' + this.level.land + (i <= 9 ? '0' : '') + i + '.png');
+        }
 
-            self.stage.addChild(background);
+        var tile_objects = [
+            "barrel_blue",
+            "barrel_blue_down",
+            "barrel_red",
+            "barrel_red_down",
+            "barrier_red",
+            "barrier_white",
+            "cone_down",
+            "cone_straight",
+            "oil",
+            "rock1",
+            "rock2",
+            "rock3",
+            "tree_large",
+            "tree_small"
+        ];
+
+        // Load all objects textures.
+        for (var i = 0; i < tile_objects.length; ++i) {
+            PIXI.loader.add(tile_objects[i], 'img/objects/' + tile_objects[i] + '.png');
+        }
+
+        PIXI.loader.load(function (loader, resources) {
+            self.stage.addChild(new PIXI.extras.TilingSprite(resources[self.level.background].texture, self.renderer.width, self.renderer.height));
+
+            // Load object textures and add them to the stage.
+            for (var i = 0; i < self.level.objects.length; ++i) {
+                var object = self.level.objects[i];
+                var objectContainer = new PIXI.Container();
+
+                var count = 0;
+
+                for (var x = 0; x < object.height; ++x) {
+                    for (var y = 0; y < object.width; ++y) {
+                        var objectTile = new PIXI.Sprite(resources[object.data[count++]].texture);
+
+                        objectTile.position.x = self.level.tile_height * y;
+                        objectTile.position.y = self.level.tile_width * x;
+
+                        objectContainer.addChild(objectTile);
+                    }
+                }
+
+                objectContainer.position.x = self.level.tile_height * object.x;
+                objectContainer.position.y = self.level.tile_width * object.y;
+
+                self.stage.addChild(objectContainer);
+            }
         });
 
         this.renderCars();
