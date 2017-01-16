06987149e2202299c679690b2ffb908920914264
Erik Verstegen <hello@erikverstegen.com>
2017-01-16 09:57:39 +0100

    Add an object layer to the level.

--- src/js/desktop/game.js
+++ src/js/desktop/game.js
@@ -20,16 +20,16 @@ module.exports = {
         var self = this;
 
         var width = level.width * 64;
-        var height = level.height * 64;
+        var height = (level.height / 2) * 64;
 
         this.renderer.autoResize = true;
         this.renderer.resize(width, height);
 
         // Load all the land textures.
         var landTypes = {
-            'grass': 14, // R-component [1-14]
-            'dirt': 14, // G-component [1-14]
-            'sand': 14 // B-component [1-14]
+            'grass': 14, // R-component [10-140]
+            'dirt': 14, // G-component [10-140]
+            'sand': 14 // B-component [10-140]
         };
 
         for (var type in landTypes) {
@@ -38,10 +38,22 @@ module.exports = {
             }
         }
 
+        // Load all the objects.
+        for (var i = 1; i <= 14; ++i) {
+            PIXI.loader.add('object_' + i, 'img/objects/object_' + (i <= 9 ? '0' : '') + i + '.png');
+        }
+
+        // Load the level.
         PIXI.loader.load(function(loader, resources) {
             for (var x = 0; x < level.width; ++x) {
-                for (var y = 0; y < level.height; ++y) {
-                    var tile = new PIXI.Sprite(resources[self.getTileForColor(level.data[x * level.height + y])].texture);
+                for (var y = 0; y < level.height / 2; ++y) {
+                    var resourceName = self.getTileForColor(level.data[x * level.height + y]);
+
+                    if (resourceName === null) {
+                        continue;
+                    }
+
+                    var tile = new PIXI.Sprite(resources[resourceName].texture);
 
                     tile.position.x = 64 * x;
                     tile.position.y = 64 * y;
@@ -50,6 +62,26 @@ module.exports = {
                     tile.scale.y = 0.5;
 
                     self.stage.addChild(tile);
+                }
+            }
+
+            for (var x = 0; x < level.width; ++x) {
+                for (var y = level.height / 2; y < level.height; ++y) {
+                    var resourceName = self.getObjectForColor(level.data[x * level.height + y]);
+
+                    if (resourceName === null) {
+                        continue;
+                    }
+
+                    var object = new PIXI.Sprite(resources[resourceName].texture);
+
+                    object.position.x = 64 * x;
+                    object.position.y = 64 * (y - level.height / 2);
+
+                    // object.scale.x = 0.5;
+                    // object.scale.y = 0.5;
+
+                    self.stage.addChild(object);
                 }
             }
         });
@@ -71,6 +103,16 @@ module.exports = {
         if (color[2] !== 0) {
             return 'sand_' + (color[2] / 10);
         }
+
+        return null;
+    },
+
+    getObjectForColor: function(color) {
+        if (color[2] !== 255) {
+            return 'object_' + (color[2] / 10);
+        }
+
+        return null;
     },
 
     renderCars: function() {
